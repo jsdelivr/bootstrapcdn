fce4ad14281b55b6c30dbcb759d696d89401003f
jdorfman <justindorfman@gmail.com>
2012-08-01 16:21:16 -0700

    some wpo & added sponsor sprint

---
+++ index.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>BootstrapCDN: Twitter Bootstrap hosted on NetDNA's Content Delivery Network</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="With NetDNA's Content Delivery Network, your Bootstrap objects will load faster, a lot faster.">
+	<meta name="author" content="Justin Dorfman">
+	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/css/bootstrap-combined.min.css" rel="stylesheet">	
+	<link href="//www.bootstrapcdn.com/css/style.origin.css?v=07302012092712" rel="stylesheet">
+	<script>
+		!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+	</script>
+
+	<!--[if lt IE 9]>
+		<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-32253110-1']);
+  _gaq.push(['_setDomainName', 'bootstrapcdn.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+	</head>
+	<body>
+
+		<div class="navbar navbar-fixed-top">
+			<div class="navbar-inner">
+				<div class="container">
+					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</a>
+					<a class="brand" href="index.html">BootstrapCDN</a>
+					<div class="nav-collapse">
+						<ul class="nav">
+							<li><a href="index.html">Home</a></li>
+							<li><a href="https://github.com/netdna/bootstrap-cdn/issues" target="_blank">Issues <i class="icon-white icon-share-alt"></i></a></li>
+							<li class="dropdown" id="menu1"><a class="dropdown-toggle" data-toggle="dropdown" href="#menu1">Reports<b class="caret"></b></a>
+								<ul class="dropdown-menu">
+									<li><a href="http://status.bootstrapcdn.com" target="_blank"><i class="icon-ok-sign"></i> Uptime</a></li>
+									<li><a href="stats.html"><i class="icon-tasks"></i> Stats/Charts</a></li>
+								</ul>
+							</li>
+						  <li><a href="http://eepurl.com/nYBcv" target="_blank">Newsletter <i class="icon-white icon-envelope"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="main">
+			<div class="container">
+				<div class="row-fluid">
+					<div class="span12">
+						<div class="hero">
+							<h1>Let us serve your Bootstrap</h1>
+							<h3>With NetDNA's Content Delivery Network, your site will load faster, a lot faster.</h3>
+						</div>
+
+						<div class="social">
+
+
+							<iframe src="http://markdotto.github.com/github-buttons/github-btn.html?user=netdna&repo=bootstrap-cdn&type=watch&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="110px" height="20px"></iframe>
+							<iframe src="http://markdotto.github.com/github-buttons/github-btn.html?user=netdna&repo=bootstrap-cdn&type=fork" allowtransparency="true" frameborder="0" scrolling="0" width="53px" height="20px"></iframe>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+							<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.bootstrapcdn.com" data-via="jdorfman">Tweet</a>
+<a href="https://twitter.com/NetDNADeveloper" class="twitter-follow-button" data-show-count="false">Follow @NetDNADeveloper</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+						</div>
+
+
+						<div class="tabbable">
+							<ul class="nav nav-tabs">
+								<li class="active"><a href="#quickstart" data-toggle="tab">QuickStart</a></li>
+								<li><a href="#css" data-toggle="tab">CSS</a></li>
+								<li><a href="#js" data-toggle="tab">JavaScript</a></li>
+								<li><a href="#img" data-toggle="tab">Images</a></li>
+								<li><a href="#ssl" data-toggle="tab">SSL</a></li>
+							</ul>
+						</div>
+
+						<!-- Actual Tab Content -->
+						<div class="tab-content">
+
+							<div class="tab-pane active" id="quickstart">
+								<h4>Simply replace your local Bootstrap objects with the ones hosted on <a href="http://www.netdna.com/technology/our-network/" target="_blank">our global network</a> of Edge Servers.</h4>
+								<div class="label-container"><span class="label">Complete CSS</span></div>
+								<p><input type="text" class="" value="//netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/css/bootstrap-combined.min.css"></p>
+
+								<div class="label-container"><span class="label">Complete JavaScript</span></div>
+								<p><input type="text" class="" value="//netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap.min.js"></p>
+							</div>	
+
+							<!-- CSS tab -->
+							<div class="tab-pane" id="css">
+								<div class="label-container"><span class="label label-info">Recommended</span></div>
+								<p><input type="text" class="recommended" value="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/css/bootstrap-combined.min.css"></p>
+								<p><input type="text" class="" value="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/css/bootstrap.min.css"> </p>
+								<p><input type="text" class="" value="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/css/bootstrap.css"></p>
+								<p><input type="text" class="" value="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/css/bootstrap-responsive.css"></p>
+								<p><input type="text" class="" value="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/css/bootstrap-responsive.min.css"></p>
+							</div>
+
+							<!-- JavaScript tab -->
+							<div class="tab-pane" id="js">
+								<div class="label-container"><span class="label label-info">Recommended</span></div>
+								<p><input type="text" class="recommended" value="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap.min.js"></p>
+								<p><input type="text" class="" value="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap.js"></p>
+
+								<h4>The following are not required if using either link above.</h4>
+								<p><input type="text" class="" value="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-alert.js"></p>
+								<p><input type="text" class="" value="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-button.js"></p>
+								<p><input type="text" class="" value="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-carousel.js"></p>
+								<p><input type="text" class="" value="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-collapse.js"></p>
+								<p><input type="text" class="" value="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-dropdown.js"></p>
+								<p><input type="text" class="" value="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-modal.js"></p>
+								<p><input type="text" class="" value="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-popover.js"></p>
+								<p><input type="text" class="" value="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-scrollspy.js"></p>
+								<p><input type="text" class="" value="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-tab.js"></p>
+								<p><input type="text" class="" value="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-tooltip.js"></p>
+								<p><input type="text" class="" value="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-transition.js"></p>
+								<p><input type="text" class="" value="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-typeahead.js"></p>
+								<p><input type="text" class="" value="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/jquery.js"></p>
+							</div>
+
+							<!-- Images Tab -->
+							<div class="tab-pane" id="img">
+								<h4>The following are required if you're already linking to CSS files.</h4>
+								<p><input type="text" class="span7" value="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/img/glyphicons-halflings.png"></p>
+								<p><input type="text" class="span7" value="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/img/glyphicons-halflings-white.png"></p>
+							</div>
+
+							<!-- SSL Tab -->			
+							<div class="tab-pane" id="ssl">
+								<h4>SSL is powered by <a href="http://www.netdna.com/products/edgessl/">EdgeSSL</a></h4>
+
+								<!-- CSS SSL -->
+								<div class="label-container"><span class="label label-info">Recommended</span></div>
+								<p><input type="text" class=" recommended" value="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/css/bootstrap-combined.min.css"></p>
+								<p><input type="text" class="" value="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/css/bootstrap.min.css"> </p>
+								<p><input type="text" class="" value="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/css/bootstrap.css"></p>
+								<p><input type="text" class="" value="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/css/bootstrap-responsive.css"></p>
+								<p><input type="text" class="" value="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/css/bootstrap-responsive.min.css"></p>
+
+								<!-- JS SSL -->
+								<div class="label-container"><span class="label label-info">Recommended</span></div>
+								<p><input type="text" class=" recommended" value="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap.min.js"></p>
+								<p><input type="text" class="" value="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap.js"></p>
+
+								<!-- Individual Files -->
+								<p><input type="text" class="" value="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-alert.js"></p>
+								<p><input type="text" class="" value="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-button.js"></p>
+								<p><input type="text" class="" value="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-carousel.js"></p>
+								<p><input type="text" class="" value="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-collapse.js"></p>
+								<p><input type="text" class="" value="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-dropdown.js"></p>
+								<p><input type="text" class="" value="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-modal.js"></p>
+								<p><input type="text" class="" value="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-popover.js"></p>
+								<p><input type="text" class="" value="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-scrollspy.js"></p>
+								<p><input type="text" class="" value="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-tab.js"></p>
+								<p><input type="text" class="" value="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-tooltip.js"></p>
+								<p><input type="text" class="" value="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-transition.js"></p>
+								<p><input type="text" class="" value="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-typeahead.js"></p>
+								<p><input type="text" class="" value="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/jquery.js"></p>
+
+								<!-- Images SSL -->
+								<p><input type="text" class="" value="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/img/glyphicons-halflings.png"></p>
+								<p><input type="text" class="" value="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/img/glyphicons-halflings-white.png"></p>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div> <!-- /container -->
+		</div>
+
+		<div class="sponsors">
+			<div class="container">
+				<div class="row-fluid">
+					<div class="span12">
+						<h3>Sponsored By:</h3>
+					</div>
+				</div>
+				<div class="row-fluid" align="center">
+						<div class="logoArea">
+							<a id="NetDNA" alt="NetDNA" target="_blank" href="http://www.netdna.com/?src=bootstrapcdn.com" data-bitly-type="bitly_hover_card"></a>
+							<a id="MaxCDN" alt="MaxCDN" target="_blank" href="http://www.maxcdn.com/?src=bootstrapcdn.com" data-bitly-type="bitly_hover_card"></a>
+							<a id="CloudCache" alt="CloudCache" target="_blank" href="http://www.cloudcache.com/?src=bootstrapcdn.com" data-bitly-type="bitly_hover_card"></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+<footer class="footer">
+			<div class="container">
+				<div class="row-fluid">
+					<div class="span12">
+						<ul class="pull-left">
+							<li>Maintained by <a href="http://www.twitter.com/jdorfman" target="_blank">@jdorfman</a>.</li>
+							<li>Designed by <a href="http://thomaspark.me" target="_blank">Thomas Park</a> of <a href="http://bootswatch.com/" target="_blank">Bootswatch</a>.</li>
+							<li>Powered by <a href="http://www.digitalfyre.com" target="_blank">DigitalFyre</a>, <a href="http://www.netdna.com" target="_blank">NetDNA</a>, and <a href="http://www.nginx.org" target="_blank">nginx</a>.</li>
+							<li>This free service comes with no warranty.</li>
+						</ul>
+						<ul class="pull-right">
+							<li>Links</li>
+							<li><a href="http://twitter.github.com/bootstrap/index.html" target="_blank">Twitter Bootstrap</a></li>
+							<li><a href="http://bootswatch.com/" target="_blank">Bootswatch</a></li>
+							<li><a href="http://builtwithbootstrap.com/" target="_blank">Built with Bootstrap</a></li>
+							<li><a href="http://www.bootstraphero.com/" target="_blank">Bootstrap Hero</a></li>
+							<li><a href="http://stylebootstrap.info/" target="_blank">StyleBootstrap</a></li>
+							<li><a href="http://bootswatchr.com/" target="_blank">Bootswatchr</a></li>
+							<li><a href="http://www.plugolabs.com/twitter-bootstrap-button-generator/" target="_blank">Bootstrap Button Generator</a></li>
+							<li><a href="http://stackoverflow.com/questions/tagged/bootstrap" target="_blank">Bootstrap Q&amp;A on Stackoverflow</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</footer>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+	<script src="//netdna.bootstrapcdn.com/js/bootstrap.min.js"></script>
+	<script>$("input:text").focus(function(){$(this).select();$(this).mouseup(function(a){a.preventDefault()})});</script>
+</body>
+</html>
