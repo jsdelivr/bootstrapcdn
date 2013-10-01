674b550af1dccd449985bbd5bb0b8a2466a5e3e2
Justin Dorfman <justindorfman@gmail.com>
2013-10-01 14:13:26 -0700

    re-added respond-proxy and updated header-check test

--- tests/header-check.sh
+++ tests/header-check.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-#todo: for loop
+#todo: clean up output, add colors
 
 time curl -I www.bootstrapcdn.com
-time curl -I netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css
-time curl -I netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.js
-time curl -I netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css
-time curl -I netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css
+time curl -I netdna.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css
+time curl -I netdna.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.js
+time curl -I netdna.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css
+time curl -I netdna.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css
+
 time curl -I netdna.bootstrapcdn.com/bootswatch/3.0.0/amelia/bootstrap.min.css
 time curl -I netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css
+
+for ext in woff ttf svg eot otf; 
+	do curl -I netdna.bootstrapcdn.com/font-awesome/latest/font/fontawesome-webfont.{$ext};
+done
+	
+	
