b584e7929fbfd48248592e42ead3385cd507e8cb
Justin Dorfman <justindorfman@gmail.com>
2013-10-01 13:32:00 -0700

    added http header check to tests

---
+++ tests/header-check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#todo: for loop
+
+time curl -I www.bootstrapcdn.com
+time curl -I netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css
+time curl -I netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.js
+time curl -I netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css
+time curl -I netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css
+time curl -I netdna.bootstrapcdn.com/bootswatch/3.0.0/amelia/bootstrap.min.css
+time curl -I netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css
