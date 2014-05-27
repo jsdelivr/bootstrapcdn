b010e0b9e3f2e92c5410c00b419cf577ca20dd7b
Gergely Nemeth <gergo@secretsaucepartners.com>
2014-05-27 08:09:15 +0200

    Seperate middleware logics to their own function

--- app.js
+++ app.js
@@ -45,13 +45,16 @@ app.disable('x-powered-by');
 
 // in line middleware actions
 app.use(function(req,res,next) {
-    // make config availabile in routes
+    // make config available in routes
     req.config = config;
-
-    // overwrite default cache-control header
-    // drop to 10 minutes
-    res.setHeader("Cache-Control", "public, max-age=600");
     next();
+});
+
+app.use(function(req, res, next) {
+  // overwrite default cache-control header
+  // drop to 10 minutes
+  res.setHeader("Cache-Control", "public, max-age=600");
+  next();
 });
 
 // middleware
