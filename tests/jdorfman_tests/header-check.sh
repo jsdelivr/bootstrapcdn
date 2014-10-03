1914bff90411c336435768248f40ca7bf1770afc
Josh Mervine <jmervine@yp.com>
2014-10-03 00:02:42 -0700

    Fixing netdna.bootstrapcdn.com vs maxcdn.bootstrapcdn.com #364

--- tests/jdorfman_tests/header-check.sh
+++ tests/jdorfman_tests/header-check.sh
@@ -8,19 +8,19 @@ time curl -I http://s3-us-west-1.amazonaws.com/bootstrap-cdn/public/index.html
 time curl -I http://s3-us-west-1.amazonaws.com/bootstrap-cdn/public/bootstrap/3.0.0/css/bootstrap.no-icons.min.css
 
 #CDN
-time curl -I netdna.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css
-time curl -I netdna.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.js
-time curl -I netdna.bootstrapcdn.com/bootstrap/latest/css/bootstrap.no-icons.min.css
+time curl -I maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css
+time curl -I maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.js
+time curl -I maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.no-icons.min.css
 
-time curl -I netdna.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css
-time curl -I netdna.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css
+time curl -I maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css
+time curl -I maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css
 
-time curl -I netdna.bootstrapcdn.com/bootswatch/3.0.0/amelia/bootstrap.min.css
-time curl -I netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css
+time curl -I maxcdn.bootstrapcdn.com/bootswatch/3.0.0/amelia/bootstrap.min.css
+time curl -I maxcdn.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css
 
 #Fonts
 for ext in woff ttf svg eot; 
-	do curl -I netdna.bootstrapcdn.com/font-awesome/latest/font/fontawesome-webfont.{$ext};
+	do curl -I maxcdn.bootstrapcdn.com/font-awesome/latest/font/fontawesome-webfont.{$ext};
 done
 	
 	
