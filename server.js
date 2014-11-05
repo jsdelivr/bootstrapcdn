6872643f9c538a38400f6e0d810f88c9dedf78ea
XhmikosR <xhmikosr@gmail.com>
2014-11-05 20:20:11 +0200

    To be TO BE SUASHED Second attempt.

--- server.js
+++ server.js
@@ -10,7 +10,15 @@ var os = require('os');
 /**
  * Setup
  **/
-var numOfCores = os.cpus().length / 2;
+var env = process.env.NODE_ENV;
+var cores = os.cpus().length;
+var numOfCores;
+if (env === "production") {
+    numOfCores = cores === 1 ? cores : cores - 1;
+} else {
+    numOfCores = cores > 1 ? cores / 2 : cores;
+}
+
 var workers = parseInt(process.env.CLUSTER_WORKERS || numOfCores, 10);
 
 cluster.setupMaster({ exec : "app.js" });
@@ -39,7 +47,7 @@ setInterval(checkRestart, 2000);
 say("Master starting:");
 say("time        => " + datefmt(new Date(), "ddd, dd mmm yyyy hh:MM:ss Z"));
 say("pid         => " + process.pid);
-say("environment => " + process.env.NODE_ENV);
+say("environment => " + env);
 
 /**
  * Fork Workers
