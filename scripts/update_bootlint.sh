6a106c800eaeb72a44081b8380031298352a328e
Josh Mervine <jmervine@yp.com>
2014-12-04 10:38:33 -0800

    Adding bootlint 0.8.0 and update script. (cc #445)

---
+++ scripts/update_bootlint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+version=$1
+if ! test "$version"; then
+  echo "Valid bootlink version required."
+  exit 1
+fi
+
+# strip leading 'v' if present
+version=`echo $version | sed 's/^v//'`
+
+if test -d public/bootlint/v$version; then
+  echo "Bootlint version already found."
+  exit 1
+fi
+
+set -uex
+mkdir public/bootlint/$version
+wget --quiet --output-document public/bootlint/$version/bootlint.js \
+  https://raw.githubusercontent.com/twbs/bootlint/v$version/dist/browser/bootlint.js
+
+exec ./scripts/lint.sh $version
