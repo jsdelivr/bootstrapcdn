5237ec1b7000fc0f910bfb05f7121c284917de1c
Josh Mervine <jmervine@heroku.com>
2015-06-16 22:03:48 -0700

    adding ie10-viewport-bug-workaround.js

---
+++ public/js/ie10-viewport-bug-workaround.js
@@ -0,0 +1,8 @@
+/*!
+ * IE10 viewport hack for Surface/desktop Windows 8 bug
+ * Copyright 2014-2015 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+ */
+!function(){"use strict"
+if(navigator.userAgent.match(/IEMobile\/10\.0/)){var e=document.createElement("style")
+e.appendChild(document.createTextNode("@-ms-viewport{width:auto!important}")),document.querySelector("head").appendChild(e)}}()
