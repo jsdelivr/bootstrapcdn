f9299e7332b509c44394c3834bde5d122dca0f19
XhmikosR <xhmikosr@gmail.com>
2016-09-01 22:24:24 +0300

    Move inline JS to main.js.

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -28,4 +28,23 @@
 
     })(window, document);
 
+    /* eslint-disable */
+    window.twttr = (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0],
+            t = window.twttr || {};
+        if (d.getElementById(id)) return t;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://platform.twitter.com/widgets.js";
+        fjs.parentNode.insertBefore(js, fjs);
+
+        t._e = [];
+        t.ready = function(f) {
+            t._e.push(f);
+        };
+
+        return t;
+    }(document, "script", "twitter-wjs"));
+    /* eslint-enable */
+
 })();
