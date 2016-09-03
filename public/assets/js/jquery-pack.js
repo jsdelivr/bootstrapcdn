9932e58f0dd9c7b2946d55082fd1beb4742749c8
XhmikosR <xhmikosr@gmail.com>
2016-09-03 21:11:48 +0300

    Remove pre IE 10 code and IE warning.

--- public/assets/js/jquery-pack.js
+++ public/assets/js/jquery-pack.js
@@ -8,17 +8,4 @@
         });
     });
 
-    $('.alert-dismissible').each(function() {
-        var $alert = $(this);
-        var alertNameLSProp = $alert.attr('data-alert-id');
-
-        if (localStorage.getItem(alertNameLSProp) === 'true') {
-            $alert.remove();
-        } else {
-            $alert.on('close.bs.alert', function() {
-                localStorage.setItem(alertNameLSProp, true);
-            });
-        }
-    });
-
 })(jQuery);
