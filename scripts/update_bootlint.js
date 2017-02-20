aad41d6f5e8764cd6a36b4342f50067b7ec90bb0
Josh Mervine <jmervine@heroku.com>
2017-02-20 23:10:14 +0200

    Fixing update_bootlint.js.

--- scripts/update_bootlint.js
+++ scripts/update_bootlint.js
@@ -11,6 +11,10 @@ var path        = require('path');
 
 var version     = process.argv[2];
 
+// strip leading 'v' if present
+version = version.replace(/^v/, '');
+
+
 var basedir     = path.join(__dirname, '..');
 var bootlintDir = path.join(basedir, 'public', 'bootlint', version);
 var UGLIFYJS    = path.join(basedir, 'node_modules/.bin/uglifyjs');
@@ -20,22 +24,13 @@ if (!version) {
     process.exit(1);
 }
 
-// strip leading 'v' if present
-version = version.replace(/^v/, '');
-
 if (test('-d', bootlintDir)) {
     echo('Bootlint version already found.');
     process.exit(1);
 }
 
 https.get('https://raw.githubusercontent.com/twbs/bootlint/v' + version + '/dist/browser/bootlint.js', function (res) {
-    mkdir(bootlintDir);
-    pushd(bootlintDir);
-
     var statusCode = res.statusCode;
-    var targetFile = path.join(bootlintDir, 'bootlint.js');
-    var targetMinFile = path.join(bootlintDir, 'bootlint.min.js');
-    var file = fs.createWriteStream(targetFile);
 
     if (statusCode !== 200) {
         console.log(new Error('Request Failed.\nStatus Code: ' + statusCode).message);
@@ -43,26 +38,33 @@ https.get('https://raw.githubusercontent.com/twbs/bootlint/v' + version + '/dist
         return;
     }
 
+    mkdir(bootlintDir);
+    pushd(bootlintDir);
+
+    var targetFile = path.join(bootlintDir, 'bootlint.js');
+    var targetMinFile = path.join(bootlintDir, 'bootlint.min.js');
+    var file = fs.createWriteStream(targetFile);
+
     res.pipe(file);
 
     res.on('end', function () {
         file.close();
+
+        exec(UGLIFYJS + ' ' + targetFile + ' -o ' + targetMinFile +
+          ' --compress --source-map ' + targetMinFile + '.map' +
+          ' --comments "/(?:^!|@(?:license|preserve|cc_on))/"');
+
+        cd('..');
+
+        if (process.platform === 'win32') {
+            fs.writeFileSync('latest', version, 'utf8');
+        } else {
+            rm('-f', 'latest');
+            ln('-sf', version, 'latest');
+        }
+
+        popd();
+
+        echo('\nDo not forget to update "config/_config.yml!"');
     });
-
-    exec(UGLIFYJS + ' ' + targetFile + ' -o ' + targetMinFile +
-      ' --compress --source-map ' + targetMinFile + '.map' +
-      ' --comments "/(?:^!|@(?:license|preserve|cc_on))/"');
-
-    cd('..');
-
-    if (process.platform === 'win32') {
-        fs.writeFileSync('latest', version, 'utf8');
-    } else {
-        ln('-sf', 'latest', version);
-    }
-
-    popd();
-
-    echo('\nDo not forget to update "config/_config.yml!"');
-
 });
