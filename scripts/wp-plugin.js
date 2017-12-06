06450ae9ae47e3a7e257667244d7855d662a2a81
XhmikosR <xhmikosr@gmail.com>
2017-12-06 14:46:57 +0200

    Fix and improve scripts/wp-plugin.js.

--- scripts/wp-plugin.js
+++ scripts/wp-plugin.js
@@ -7,32 +7,38 @@ const path   = require('path');
 const yaml   = require('js-yaml');
 const config = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, '..', 'config', '_config.yml'), 'utf8'));
 
+const OUT_DIR = path.resolve(__dirname, '..', 'data');
+
 const data = {
     timestamp: new Date(),
     bootstrap: {},
     fontawesome: {}
 };
 
-config.bootstrap.forEach((bootstrap) => {
+config.bootstrap3.forEach((bootstrap) => {
     data.bootstrap[bootstrap.version] = {
-        css: bootstrap.css_complete,
-        js:  bootstrap.javascript
+        css: bootstrap.stylesheet,
+        js: bootstrap.javascript
     };
 });
 
 config.fontawesome.forEach((fontawesome) => {
-    data.fontawesome[fontawesome.version] = fontawesome.css_complete;
+    data.fontawesome[fontawesome.version] = fontawesome.stylesheet;
 });
 
+if (!fs.existsSync(OUT_DIR)) {
+    fs.mkdirSync(OUT_DIR);
+}
+
 fs.writeFile(
-    path.resolve(__dirname, '..', 'public', 'data', 'bootstrapcdn.json'),
+    path.resolve(OUT_DIR, 'bootstrapcdn.json'),
     JSON.stringify(data, null, 2),
     (err) => {
         if (err) {
-            console.error('an error occured creating bootstrapcdn.json');
+            console.error('An error occured creating bootstrapcdn.json');
             console.trace(err);
             process.exit(1);
         }
-        console.log('regenerated bootstrapcdn.json');
+        console.log('Regenerated bootstrapcdn.json');
     }
 );
