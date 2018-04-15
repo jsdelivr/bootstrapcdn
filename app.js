1ffd229af8c7aa6b33d7ec61c4d7b87eb29670da
XhmikosR <xhmikosr@gmail.com>
2018-04-15 16:05:23 +0300

    Move CSP config to config/helmet-csp.js.

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ const staticify    = require('staticify')(PUBLIC_DIR, {
     sendOptions: STATIC_OPTS
 });
 
+const CSP          = require('./config/helmet-csp.js');
 const helpers      = require('./lib/helpers.js');
 const routes       = require('./routes');
 
@@ -132,73 +133,7 @@ app.use(helmet.hsts({
 app.use(helmet.referrerPolicy({ policy: 'strict-origin-when-cross-origin' }));
 
 app.use(helmet.contentSecurityPolicy({
-    directives: {
-        defaultSrc: ['\'none\''],
-        baseUri: ['\'self\''],
-        formAction: ['syndication.twitter.com'],
-        frameAncestors: ['\'none\''],
-        scriptSrc: [
-            '\'self\'',
-            '\'unsafe-inline\'',
-            'stackpath.bootstrapcdn.com',
-            'www.google-analytics.com',
-            'code.jquery.com',
-            'platform.twitter.com',
-            'cdn.syndication.twimg.com',
-            'api.github.com',
-            'cdn.carbonads.com',
-            'srv.carbonads.net',
-            'adn.fusionads.net',
-            'fallbacks.carbonads.com',
-            (req, res) => `'nonce-${res.locals.nonce}'`
-        ],
-        styleSrc: [
-            '\'self\'',
-            '\'unsafe-inline\'',
-            'stackpath.bootstrapcdn.com',
-            'fonts.googleapis.com',
-            '*.twimg.com',
-            'platform.twitter.com'
-        ],
-        imgSrc: [
-            '\'self\'',
-            'data:',
-            'www.google-analytics.com',
-            'bootswatch.com',
-            '*.twitter.com',
-            '*.twimg.com',
-            'stats.g.doubleclick.net',
-            'srv.carbonads.net',
-            'assets.servedby-buysellads.com',
-            'ad.doubleclick.net',
-            '*.convertro.com',
-            '*.c3tag.com',
-            '*.2mdn.net',
-            'launchbit.com',
-            'www.launchbit.com'
-        ],
-        fontSrc: [
-            '\'self\'',
-            'stackpath.bootstrapcdn.com',
-            'fonts.gstatic.com'
-        ],
-        frameSrc: [
-            '\'self\'',
-            'platform.twitter.com',
-            'syndication.twitter.com',
-            'ghbtns.com'
-        ],
-        childSrc: [
-            '\'self\'',
-            'platform.twitter.com',
-            'syndication.twitter.com',
-            'ghbtns.com'
-        ],
-        connectSrc: [
-            'syndication.twitter.com'
-        ],
-        manifestSrc: ['\'self\'']
-    },
+    directives: CSP,
 
     // This module will detect common mistakes in your directives and throw errors
     // if it finds any. To disable this, enable "loose mode".
