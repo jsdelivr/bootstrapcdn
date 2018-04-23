c39d46765c3440e06f67fca8da679da660489cfe
XhmikosR <xhmikosr@gmail.com>
2018-04-23 21:37:43 +0300

    Add a script to update Bootstrap.
    We load Bootstrap from npm, so, make sure you update the version there and do `npm i` before running the script to add a new version.

---
+++ scripts/update_bootstrap.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const fs = require('fs');
+const fse = require('fs-extra');
+const path = require('path');
+
+let version = process.argv[2];
+
+if (!version) {
+    console.log('Valid Bootstrap version required.');
+    process.exit(1);
+}
+
+// strip leading 'v' if present
+version = version.replace(/^v/, '');
+
+const basedir = path.join(__dirname, '..');
+const bootstrapSrcDir = path.join(basedir, 'node_modules/bootstrap/dist');
+const bootstrapDistDir = path.join(basedir, 'public', 'twitter-bootstrap', version);
+
+if (fs.existsSync(bootstrapDistDir)) {
+    console.log('Bootstrap version already found.');
+    process.exit(1);
+}
+
+fs.mkdirSync(bootstrapDistDir);
+
+try {
+    fse.copySync(`${bootstrapSrcDir}`, `${bootstrapDistDir}`, {
+        overwrite: false,
+        errorOnExist: true,
+        preserveTimestamps: true
+    });
+    console.log(`Successfully copied "${bootstrapSrcDir}" to "${bootstrapDistDir}"`);
+    console.log(`\nDo not forget to update "${path.normalize('config/_config.yml')}"!`);
+} catch (err) {
+    console.log(err);
+}
