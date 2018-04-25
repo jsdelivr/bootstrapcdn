e0834e185495b583a509e098d1cd5375ab215756
XhmikosR <xhmikosr@gmail.com>
2018-04-25 23:00:02 +0300

    test/index_test.js: Add missing `describe()`.

--- test/index_test.js
+++ test/index_test.js
@@ -15,65 +15,67 @@ before((done) => {
     });
 });
 
-it('works', (done) => {
-    helpers.assert.itWorks(response.statusCode, done);
-});
+describe('index', () => {
+    it('works', (done) => {
+        helpers.assert.itWorks(response.statusCode, done);
+    });
 
-const current = config.bootstrap[0];
+    const current = config.bootstrap[0];
 
-it('is current', (done) => {
-    assert.ok(current.current);
-    done();
-});
-
-it('valid html', (done) => {
-    helpers.assert.validHTML(response, done);
-});
-
-it('contains authors', (done) => {
-    helpers.assert.authors(response, done);
-});
-
-it('has page header', (done) => {
-    helpers.assert.pageHeader('Quick Start', response, done);
-});
-
-describe('stylesheet', () => {
-    it('has uri', (done) => {
-        assert.ok(response.body.includes(current.stylesheet),
-            `Expects response body to include "${current.stylesheet}"`);
+    it('is current', (done) => {
+        assert.ok(current.current);
         done();
     });
 
-    ['html', 'pug', 'haml'].forEach((fmt) => {
-        it(`has ${fmt}`, (done) => {
-            const str = helpers.css[fmt](current.stylesheet, current.stylesheetSri);
+    it('valid html', (done) => {
+        helpers.assert.validHTML(response, done);
+    });
 
-            assert.ok(response.body.includes(str), `Expects response body to include "${str}"`);
+    it('contains authors', (done) => {
+        helpers.assert.authors(response, done);
+    });
+
+    it('has page header', (done) => {
+        helpers.assert.pageHeader('Quick Start', response, done);
+    });
+
+    describe('stylesheet', () => {
+        it('has uri', (done) => {
+            assert.ok(response.body.includes(current.stylesheet),
+                `Expects response body to include "${current.stylesheet}"`);
             done();
         });
-    });
-});
 
-describe('javascript', () => {
-    it('has javascript uri', (done) => {
-        assert.ok(response.body.includes(current.javascript),
-            `Expects response body to include "${current.javascript}"`);
-        done();
-    });
+        ['html', 'pug', 'haml'].forEach((fmt) => {
+            it(`has ${fmt}`, (done) => {
+                const str = helpers.css[fmt](current.stylesheet, current.stylesheetSri);
 
-    it('has javascript bundle uri', (done) => {
-        assert.ok(response.body.includes(current.javascriptBundle),
-            `Expects response body to include "${current.javascriptBundle}"`);
-        done();
+                assert.ok(response.body.includes(str), `Expects response body to include "${str}"`);
+                done();
+            });
+        });
     });
 
-    ['html', 'pug', 'haml'].forEach((fmt) => {
-        it(`has ${fmt}`, (done) => {
-            const str = helpers.javascript[fmt](current.javascript, current.javascriptSri);
-
-            assert.ok(response.body.includes(str), `Expects response body to include "${str}"`);
+    describe('javascript', () => {
+        it('has javascript uri', (done) => {
+            assert.ok(response.body.includes(current.javascript),
+                `Expects response body to include "${current.javascript}"`);
             done();
+        });
+
+        it('has javascript bundle uri', (done) => {
+            assert.ok(response.body.includes(current.javascriptBundle),
+                `Expects response body to include "${current.javascriptBundle}"`);
+            done();
+        });
+
+        ['html', 'pug', 'haml'].forEach((fmt) => {
+            it(`has ${fmt}`, (done) => {
+                const str = helpers.javascript[fmt](current.javascript, current.javascriptSri);
+
+                assert.ok(response.body.includes(str), `Expects response body to include "${str}"`);
+                done();
+            });
         });
     });
 });
