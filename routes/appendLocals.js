2d09bfa4bfd841b59cf678956e5d7f71f880279c
XhmikosR <xhmikosr@gmail.com>
2018-05-21 22:28:17 +0300

    routes/appendLocals.js: refactor for readability.
    Also,
    
    * move page title suffix to config
    * fix empty body class for the index page

--- routes/appendLocals.js
+++ routes/appendLocals.js
@@ -5,46 +5,78 @@ const helpers = require('../lib/helpers.js');
 const config = require('../config');
 
 const digest = helpers.sri.digest;
+const PUBLIC_DIR = path.join(__dirname, '../public/');
 const SRI_CACHE = {};
 
-function appendLocals(req, res) {
-    const totalThemes = config.bootswatch4.themes.length;
-    const TITLE_SUFFIX = 'BootstrapCDN by StackPath';
-    const pageUrl = req.originalUrl;
+function getProto(req) {
     let proto = req.get('x-forwarded-proto');
 
     if (typeof proto === 'undefined') {
         proto = req.protocol;
     }
 
-    res.locals.canonicalUrl = `${req.config.siteurl}${req.path}`;
+    return proto;
+}
 
-    res.locals.siteUrl = `${proto}://${req.hostname}`;
-    res.locals.pageUrl = pageUrl;
+function getCurrentSiteurl(req) {
+    const proto = getProto(req);
 
-    res.locals.theme = req.query.theme < totalThemes ?
+    return `${proto}://${req.hostname}`;
+}
+
+function getPageTitle(pageTitle) {
+    return `${pageTitle} · ${config.title_suffix}`;
+}
+
+function getThemeQuery(req) {
+    const totalThemes = config.bootswatch4.themes.length;
+
+    return req.query.theme < totalThemes ?
         req.query.theme :
         '';
+}
 
-    res.locals.displayTitle = (title) => `${title} · ${TITLE_SUFFIX}`;
+function generateBodyClass(url) {
+    let str = url;
 
-    res.locals.bodyClass = () => {
-        let str = pageUrl.replace(/\s/g, '') // remove any whitespace
-                    .replace(/\//g, ''); // remove any slashes
+    if (str === '/') {
+        str = 'home'; // only for the index page
+    }
 
-        // Make the first letter lowercase
-        str = str.charAt(0).toLowerCase() + str.slice(1);
+    str = str.replace(/\//g, ''); // remove any slashes
 
-        return `page-${str}`;
+    // Make the first letter lowercase
+    str = str.charAt(0).toLowerCase() + str.slice(1);
+
+    return `page-${str}`;
+}
+
+function generateSRI(file) {
+    if (typeof SRI_CACHE[file] === 'undefined') {
+        SRI_CACHE[file] = digest(path.join(PUBLIC_DIR, file));
+    }
+
+    return SRI_CACHE[file];
+}
+
+function appendLocals(req, res) {
+    const siteUrl = getCurrentSiteurl(req);
+    const canonicalUrl = `${config.siteurl}${req.path}`;
+    const theme = getThemeQuery(req);
+    const pageUrl = req.originalUrl;
+    const bodyClass = generateBodyClass(pageUrl);
+
+    const locals = {
+        siteUrl,
+        canonicalUrl,
+        pageUrl,
+        theme,
+        displayTitle: getPageTitle,
+        bodyClass,
+        generateSRI
     };
 
-    res.locals.generateSRI = (file) => {
-        if (typeof SRI_CACHE[file] === 'undefined') {
-            SRI_CACHE[file] = digest(path.join(__dirname, '../public', file));
-        }
-
-        return SRI_CACHE[file];
-    };
+    res.locals = Object.assign(res.locals, locals);
 
     return res;
 }
