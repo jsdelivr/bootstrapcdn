70f45d40da00626710245dac99aa4684cabc4e7e
XhmikosR <xhmikosr@gmail.com>
2019-03-09 17:04:00 +0200

    Throw an error instead of using `process.exit(1)`.

--- scripts/update_bootlint.js
+++ scripts/update_bootlint.js
@@ -6,35 +6,36 @@ const fs = require('fs');
 const path = require('path');
 const fse = require('fs-extra');
 
-let version = process.argv[2];
+(() => {
+    let version = process.argv[2];
 
-if (!version) {
-    console.log('Valid Bootlint version required.');
-    process.exit(1);
-}
+    if (!version) {
+        throw new Error('Valid Bootlint version required.\nPlease pass the Bootlint version as an argument.');
+    }
 
-// strip leading 'v' if present
-version = version.replace(/^v/, '');
+    // strip leading 'v' if present
+    version = version.replace(/^v/, '');
 
-const basedir = path.join(__dirname, '..');
-const bootlintSrcDir = path.join(basedir, 'node_modules/bootlint/dist/browser/');
-const bootlintDistDir = path.join(basedir, 'cdn', 'bootlint', version);
+    const basedir = path.join(__dirname, '..');
+    const bootlintSrcDir = path.join(basedir, 'node_modules/bootlint/dist/browser/');
+    const bootlintDistDir = path.join(basedir, 'cdn', 'bootlint', version);
 
-if (fs.existsSync(bootlintDistDir)) {
-    console.log('Bootlint version already found.');
-    process.exit(1);
-}
+    if (fs.existsSync(bootlintDistDir)) {
+        console.warn('Bootlint version already found, exiting.');
+        return;
+    }
 
-fs.mkdirSync(bootlintDistDir);
+    fs.mkdirSync(bootlintDistDir);
 
-try {
-    fse.copySync(`${bootlintSrcDir}`, `${bootlintDistDir}`, {
-        overwrite: false,
-        errorOnExist: true,
-        preserveTimestamps: true
-    });
-    console.log(`Successfully copied "${bootlintSrcDir}" to "${bootlintDistDir}"`);
-    console.log(`\nDo not forget to update "${path.normalize('config/_config.yml')}"!`);
-} catch (error) {
-    throw new Error(error);
-}
+    try {
+        fse.copySync(`${bootlintSrcDir}`, `${bootlintDistDir}`, {
+            overwrite: false,
+            errorOnExist: true,
+            preserveTimestamps: true
+        });
+        console.log(`Successfully copied "${bootlintSrcDir}" to "${bootlintDistDir}"`);
+        console.log(`\nDo not forget to update "${path.normalize('config/_config.yml')}"!`);
+    } catch (error) {
+        throw new Error(error);
+    }
+})();
--- scripts/update_bootstrap.js
+++ scripts/update_bootstrap.js
@@ -6,35 +6,36 @@ const fs = require('fs');
 const path = require('path');
 const fse = require('fs-extra');
 
-let version = process.argv[2];
+(() => {
+    let version = process.argv[2];
 
-if (!version) {
-    console.log('Valid Bootstrap version required.');
-    process.exit(1);
-}
+    if (!version) {
+        throw new Error('Valid Bootstrap version required.\nPlease pass the Bootstrap version as an argument.');
+    }
 
-// strip leading 'v' if present
-version = version.replace(/^v/, '');
+    // strip leading 'v' if present
+    version = version.replace(/^v/, '');
 
-const basedir = path.join(__dirname, '..');
-const bootstrapSrcDir = path.join(basedir, 'node_modules/bootstrap/dist');
-const bootstrapDistDir = path.join(basedir, 'cdn', 'twitter-bootstrap', version);
+    const basedir = path.join(__dirname, '..');
+    const bootstrapSrcDir = path.join(basedir, 'node_modules/bootstrap/dist');
+    const bootstrapDistDir = path.join(basedir, 'cdn', 'twitter-bootstrap', version);
 
-if (fs.existsSync(bootstrapDistDir)) {
-    console.log('Bootstrap version already found.');
-    process.exit(1);
-}
+    if (fs.existsSync(bootstrapDistDir)) {
+        console.warn('Bootstrap version already found, exiting.');
+        return;
+    }
 
-fs.mkdirSync(bootstrapDistDir);
+    fs.mkdirSync(bootstrapDistDir);
 
-try {
-    fse.copySync(`${bootstrapSrcDir}`, `${bootstrapDistDir}`, {
-        overwrite: false,
-        errorOnExist: true,
-        preserveTimestamps: true
-    });
-    console.log(`Successfully copied "${bootstrapSrcDir}" to "${bootstrapDistDir}"`);
-    console.log(`\nDo not forget to update "${path.normalize('config/_config.yml')}"!`);
-} catch (error) {
-    throw new Error(error);
-}
+    try {
+        fse.copySync(`${bootstrapSrcDir}`, `${bootstrapDistDir}`, {
+            overwrite: false,
+            errorOnExist: true,
+            preserveTimestamps: true
+        });
+        console.log(`Successfully copied "${bootstrapSrcDir}" to "${bootstrapDistDir}"`);
+        console.log(`\nDo not forget to update "${path.normalize('config/_config.yml')}"!`);
+    } catch (error) {
+        throw new Error(error);
+    }
+})();
