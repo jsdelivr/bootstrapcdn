36c2865bdfbc42242bed6906d0ae8d438bbed11c
XhmikosR <xhmikosr@gmail.com>
2019-07-19 11:14:30 +0300

    Load Twitter script via a script tag.

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -59,28 +59,6 @@
         });
     }
 
-    function loadTwitterScript() {
-        /* eslint-disable */
-        window.twttr = (function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0],
-                t = window.twttr || {};
-            if (d.getElementById(id)) return t;
-            js = d.createElement(s);
-            js.id = id;
-            js.src = 'https://platform.twitter.com/widgets.js';
-            fjs.parentNode.insertBefore(js, fjs);
-
-            t._e = [];
-            t.ready = function(f) {
-                t._e.push(f);
-            };
-
-            return t;
-        }(document, 'script', 'twitter-wjs'));
-        /* eslint-enable */
-    }
-
-    loadTwitterScript();
     toggleInputCaret();
     selectTextCopyToClipboard();
 })();
