f11fd5dd31b9e604872506fca3c78fb4bb271e66
Josh Mervine <jmervine@heroku.com>
2020-05-26 11:57:39 -0500

    initial commit of uploader automation

---
+++ scripts/uploader/content_type_map.rb
@@ -0,0 +1,16 @@
+CONTENT_TYPE_MAP = {
+    "css"   => "text/css; charset=utf-8",
+    "eot"   => "application/vnd.ms-fontobject",
+    "html"  => "text/html; charset=utf-8",
+    "js"    => "text/javascript; charset=utf-8",
+    "json"  => "application/json; charset=utf-8",
+    "less"  => "text/plain",
+    "map"   => "application/json; charset=utf-8",
+    "otf"   => "font/otf",
+    "png"   => "image/png",
+    "scss"  => "text/x-scss",
+    "svg"   => "image/svg+xml",
+    "ttf"   => "font/ttf",
+    "woff"  => "font/woff",
+    "woff2" => "font/woff2"
+}
