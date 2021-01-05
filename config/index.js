a33d4f0067fa5d3fc4ce23a2b3a59cc07bb4a9a5
XhmikosR <xhmikosr@gmail.com>
2021-01-05 21:45:30 +0200

    Update files for js-yaml 4.x.

--- config/index.js
+++ config/index.js
@@ -5,7 +5,7 @@ const path = require('path');
 const yaml = require('js-yaml');
 
 function loadConfig(file) {
-    return yaml.safeLoad(fs.readFileSync(path.join(__dirname, file)), 'utf8');
+    return yaml.load(fs.readFileSync(path.join(__dirname, file)), 'utf8');
 }
 
 ['_app.yml', '_extras.yml', '_files.yml'].forEach((file) => {
