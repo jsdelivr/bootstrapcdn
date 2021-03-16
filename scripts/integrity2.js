d460b185f951e3c1fbaa4e423f977ede2393e33c
anta99 <antanaskovic99@gmail.com>
2021-03-16 23:06:06 +0100

    Made yml file

--- scripts/integrity2.js
+++ scripts/integrity2.js
@@ -1,206 +1,105 @@
+#!/usr/bin/env node
+
 'use strict'
 
-const axios = require('axios').default
+const fs = require('fs')
+const path = require('path')
+const yaml = require('js-yaml')
+const { files } = require('../config')
+const { generateSri } = require('../lib/helpers')
 
-const apiURL = 'https://data.jsdelivr.com/v1/package/npm'
-const baseURL = `https://cdn.jsdelivr.net/npm/`
-const packagesNameList = ['font-awesome', 'bootstrap', 'bootswatch', 'bootlint']
+const { configFile } = require('./generateFiles')
 
-async function getPackage(packageName) {
-    const { data } = await axios.get(`${apiURL}/${packageName}`)
-    return { ...data, packageName }
+function buildPath(dir) {
+    dir = dir
+        .replace('/bootstrap/', '/twitter-bootstrap/')
+        .replace('https://stackpath.bootstrapcdn.com/', '')
+
+    return path.join(__dirname, '../cdn', dir)
 }
 
-function findFile(folder, filename) {
-    const file = folder.files.find((file) => file.name == filename)
-    return file
-}
+function exists(file) {
+    const found = fs.existsSync(file)
 
-function makePackagesObject(packsArr) {
-    const packagesNameObj = {
-        'font-awesome': [],
-        bootstrap: [],
-        bootswatch: [],
-        bootlint: [],
+    if (!found) {
+        console.warn(`WARNING: ${file} not found`)
     }
-    packsArr.map((pack) => {
-        packagesNameObj[pack.n] = pack.v
-    })
-    return packagesNameObj
+
+    return found
 }
 
-function buildPath(packageData, ext, filename) {
-    let path = `${baseURL}${packageData.packageName}@${packageData.version}/`
-    const dir = findFile(packageData, 'dist')
-    if (dir) {
-        path += dir.name
-        const extensionFolder = findFile(dir, ext)
-        if (extensionFolder) {
-            path += `/${extensionFolder.name}`
-            const file = findFile(extensionFolder, filename)
-            if (file) {
-                path += `/${file.name}`
-            } else {
-                return undefined
-            }
-        }
-        return path
-    }
-    return false
-}
+// // Bootswatch {3,4}
+// ;['bootswatch3', 'bootswatch4'].forEach((key) => {
+//     const bootswatch = buildPath(files[key].bootstrap)
 
-function buildPathFontAwesome(packageData) {
-    let path = `${baseURL}${packageData.packageName}@${packageData.version}/`
-    const cssFolder = findFile(packageData, 'css')
-    path += cssFolder.name
-    const cssFile = findFile(cssFolder, 'font-awesome.min.css')
-    path += `/${cssFile.name}`
-    return path
-}
+//     files[key].themes.forEach((theme) => {
+//         const file = bootswatch
+//             .replace('SWATCH_VERSION', files[key].version)
+//             .replace('SWATCH_NAME', theme.name)
 
-function buildPathBootsWatch(packageData) {
-    const basepath = `${baseURL}${packageData.packageName}@${packageData.version}`
-    const distFolder = findFile(packageData, 'dist')
-    let themes
-    if (distFolder) {
-        let path = `${basepath}/${distFolder.name}`
-        themes = distFolder.files.map((theme) => {
-            const name = theme.name
-            const cssFile = findFile(theme, 'bootstrap.min.css').name
-            return {
-                name: name,
-                stylesheet: `${path}/${name}/${cssFile}`,
-            }
-        })
-    } else {
-        const themesNames = [
-            'cerulean',
-            'cosmo',
-            'cyborg',
-            'darkly',
-            'flatly',
-            'journal',
-            'litera',
-            'lumen',
-            'lux',
-            'materia',
-            'minty',
-            'pulse',
-            'sandstone',
-            'simplex',
-            'sketchy',
-            'slate',
-            'solar',
-            'spacelab',
-            'superhero',
-            'united',
-            'yeti',
-        ]
-        themes = themesNames
-            .map((theme) => {
-                const themeFolder = findFile(packageData, theme)
-                if (themeFolder) {
-                    let path = `${basepath}/${themeFolder.name}`
-                    const cssFile = findFile(themeFolder, 'bootstrap.min.css')
-                    path += `/${cssFile.name}`
-                    return { name: theme, stylesheet: path }
-                }
-            })
-            .filter((und) => und)
-    }
-    return themes
-}
-
-function buildPathBootlint(packageData) {
-    return packageData.default
-        ? `${baseURL}/${packageData.packageName}@${packageData.version}${packageData.default}`
-        : false
-}
-
-async function onPackageVersions({ versions, packageName }) {
-    try {
-        const promises = versions.map(async (version) => {
-            const res = await getPackage(`${packageName}@${version}`)
-            return { ...res, version, packageName }
-        })
-
-        const packages = await Promise.all(promises)
-
-        const cdn = packages
-            .map((pack) => {
-                const paths = { version: pack.version }
-                //Start switch
-                switch (pack.packageName) {
-                    case 'bootstrap':
-                        paths.stylesheet = buildPath(
-                            pack,
-                            'css',
-                            'bootstrap.min.css',
-                        )
-                        paths.javascript = buildPath(
-                            pack,
-                            'js',
-                            'bootstrap.min.js',
-                        )
-                        paths.javascriptBundle = buildPath(
-                            pack,
-                            'js',
-                            'bootstrap.bundle.min.js',
-                        )
-                        paths.javascriptEsm = buildPath(
-                            pack,
-                            'js',
-                            'bootstrap.esm.min.js',
-                        )
-
-                        Object.keys(paths).map((path) => {
-                            !paths[path] && delete paths[path]
-                        })
-                        break
-                    case 'bootswatch':
-                        paths.themes = buildPathBootsWatch(pack)
-                        break
-                    case 'font-awesome':
-                        paths.stylesheet = buildPathFontAwesome(pack)
-                        break
-                    case 'bootlint':
-                        const bootlintPath = buildPathBootlint(pack)
-                        if (bootlintPath) {
-                            paths.javascript = buildPathBootlint(pack)
-                        } else {
-                            return false
-                        }
-                        break
-                    default:
-                        return null
-                }
-                //End switch
-                return paths
-            })
-            .filter((cdn) => cdn)
-
-        return cdn
-    } catch (error) {
-        console.error(error)
-    }
-}
-
-// getPackage('font-awesome')
-//     .then(onPackageVersions)
-//     .catch((err) => {
-//         console.log(err.message)
+//         if (exists(file)) {
+//             theme.sri = generateSri(file)
+//         }
 //     })
-const allPaths = packagesNameList.map(async (p) => {
-    const versions = await getPackage(p)
-    const packPaths = await onPackageVersions(versions)
-    return { n: p, v: packPaths }
+// })
+
+// // Bootlint
+// files.bootlint.forEach((bootlint) => {
+//     const file = buildPath(bootlint.javascript)
+
+//     if (exists(file)) {
+//         bootlint.javascriptSri = generateSri(file)
+//     }
+// })
+
+// // Bootstrap
+// files.bootstrap.forEach((bootstrap) => {
+//     const javascript = buildPath(bootstrap.javascript)
+//     const stylesheet = buildPath(bootstrap.stylesheet)
+//     let { javascriptBundle, javascriptEsm } = bootstrap
+
+//     if (javascriptBundle) {
+//         javascriptBundle = buildPath(bootstrap.javascriptBundle)
+//     }
+
+//     if (javascriptEsm) {
+//         javascriptEsm = buildPath(bootstrap.javascriptEsm)
+//     }
+
+//     if (exists(javascript)) {
+//         bootstrap.javascriptSri = generateSri(javascript)
+//     }
+
+//     if (javascriptBundle && exists(javascriptBundle)) {
+//         bootstrap.javascriptBundleSri = generateSri(javascriptBundle)
+//     }
+
+//     if (javascriptEsm && exists(javascriptEsm)) {
+//         bootstrap.javascriptEsmSri = generateSri(javascriptEsm)
+//     }
+
+//     if (exists(stylesheet)) {
+//         bootstrap.stylesheetSri = generateSri(stylesheet)
+//     }
+// })
+
+// Font Awesome
+files.fontawesome.forEach((fontawesome) => {
+    const stylesheet = buildPath(fontawesome.stylesheet)
+
+    if (exists(stylesheet)) {
+        fontawesome.stylesheetSri = generateSri(stylesheet)
+    }
 })
 
-Promise.all(allPaths)
-    .then((res) => {
-        const packageObj = makePackagesObject(res)
-        console.log(packageObj)
-    })
-    .catch((err) => {
-        console.log(err.message)
-    })
+// Create backup file
+fs.copyFileSync(configFile, `${configFile}.bak`)
+
+fs.writeFileSync(
+    configFile,
+    yaml.dump(files, {
+        lineWidth: -1,
+    }),
+)
+
+console.log(configFile)
