c2ffd1a5dc9486a2478408638dc3d831410b21ac
xbpcb <ch.kyrill@gmail.com>
2021-06-30 14:03:55 +0300

    Use rest params instead of arguments

--- public/assets/js/google-tag-manager.js
+++ public/assets/js/google-tag-manager.js
@@ -2,10 +2,9 @@
     'use strict';
 
     window.dataLayer = window.dataLayer || [];
-    function gtag() {
-        window.dataLayer.push(arguments);
+    function gtag(...params) {
+        window.dataLayer.push(params);
     }
-
     gtag('js', new Date());
     gtag('config', 'G-WWCYVX0YTQ');
 })();
